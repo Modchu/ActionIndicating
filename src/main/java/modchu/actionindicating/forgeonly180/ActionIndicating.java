04a99dee6f53e5dff3b85369e362bf24f27d8950
modchu <kawd82jstskl@yahoo.co.jp>
2015-01-18 18:15:58 +0900

    -

---
+++ src/main/java/modchu/actionindicating/forgeonly180/ActionIndicating.java
@@ -0,0 +1,8 @@
+package modchu.actionindicating.forgeonly180;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = "ActionIndicating", name = "ActionIndicating", version = "dummy180")
+public class ActionIndicating {
+	// assets読み込み用ダミー
+}
