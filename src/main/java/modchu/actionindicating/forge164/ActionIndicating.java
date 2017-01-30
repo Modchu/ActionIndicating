40e7bec1a6d021c39a5db5b27928f0a1f70dd7bd
Modchu <kawd82jstskl@yahoo.co.jp>
2017-01-30 13:35:48 +0900

    v6b

---
+++ src/main/java/modchu/actionindicating/forge164/ActionIndicating.java
@@ -0,0 +1,8 @@
+package modchu.actionindicating.forge164;
+
+import cpw.mods.fml.common.Mod;
+
+@Mod(modid = "actionindicating", name = "ActionIndicating", version = "dummy164", acceptedMinecraftVersions="")
+public class ActionIndicating {
+	// assets読み込み用ダミー
+}
---
+++ src/main/java/modchu/actionindicating/forge180/ActionIndicating.java
@@ -0,0 +1,8 @@
+package modchu.actionindicating.forge180;
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod(modid = "actionindicating", name = "ActionIndicating", version = "dummy164", acceptedMinecraftVersions="")
+public class ActionIndicating {
+	// assets読み込み用ダミー
+}
